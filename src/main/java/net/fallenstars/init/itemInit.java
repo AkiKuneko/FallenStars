a7e22bb01fa32bd399caaa49b961fcf6eb78f75f
RecallGGs <recallggs@gmail.com>
2020-08-27 15:05:51 -0500

    ReWorked LootTable for Sun Temples to Prepare for Moon Temples New Record

--- src/main/java/net/fallenstars/init/itemInit.java
+++ src/main/java/net/fallenstars/init/itemInit.java
@@ -1,7 +1,6 @@
 package net.fallenstars.init;
 
 import net.fallenstars.item.ItemMoonRecord;
-import net.fallenstars.objects.items.ItemBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemRecord;
 
@@ -11,6 +10,9 @@ import java.util.List;
 public class itemInit
 {
     public static final List<Item> ITEMS = new ArrayList<Item>();
+
+    public static final ItemRecord closetothesun_record = new ItemMoonRecord("sundisc", musicInit.closetothesun,"closetothesun_record","itemclosetothesunrecord");
+
 
     public static final ItemRecord moonlightsonata_record = new ItemMoonRecord("moondisc", musicInit.moonlightsonata,"moonlightsonata_record","itemmoonlightsonatarecord");
 }
--- src/main/java/net/fallenstars/init/musicInit.java
+++ src/main/java/net/fallenstars/init/musicInit.java
@@ -9,9 +9,11 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 public class musicInit {
 
     public static SoundEvent moonlightsonata;
+    public static SoundEvent closetothesun;
 
     public static void registerSounds(){
         moonlightsonata=registerSound("moonlightsonata");
+        closetothesun=registerSound("closetothesun");
     }
     private static SoundEvent registerSound(String soundName){
         final SoundEvent sound = new SoundEvent(new ResourceLocation(FallenStars.MODID, soundName)).setRegistryName(new ResourceLocation(FallenStars.MODID,soundName));
