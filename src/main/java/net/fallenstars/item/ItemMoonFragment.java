e29dce85ac3498647c8236db43de2d7ac4acda68
RecallGGs <recallggs@gmail.com>
2020-08-27 13:49:27 -0500

    Moon Fragment Full implement + recipes

---
+++ src/main/java/net/fallenstars/item/ItemMoonFragment.java
@@ -0,0 +1,68 @@
+
+package net.fallenstars.item;
+
+import net.fallenstars.ElementsFallenStars;
+import net.fallenstars.creativetab.TabFallenStars;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+
+@ElementsFallenStars.ModElement.Tag
+public class ItemMoonFragment extends ElementsFallenStars.ModElement {
+	@GameRegistry.ObjectHolder("fallenst:moonfragment")
+	public static final Item block = null;
+	public ItemMoonFragment(ElementsFallenStars instance) {
+		super(instance, 21);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("fallenst:moonfragment", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 64;
+			setUnlocalizedName("moonfragment");
+			setRegistryName("moonfragment");
+			setCreativeTab(TabFallenStars.tab);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<String> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add("Tier 1 crafting compenent");
+		}
+	}
+}
