c70e63abfe0949e2b21e2b9e6cba801a676ac505
RecallGGs <recallggs@gmail.com>
2020-09-05 18:43:44 -0500

    Fixed Star Skipper Spawns

--- src/main/java/net/fallenstars/commonProxy.java
+++ src/main/java/net/fallenstars/commonProxy.java
@@ -5,6 +5,7 @@ import net.fallenstars.gen.WorldGenCustomeStructures;
 import net.fallenstars.init.EntityInit;
 import net.fallenstars.init.musicInit;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
@@ -26,6 +27,9 @@ public class commonProxy {
             ls.add(itr.next());
         return ls.toArray(new Biome[ls.size()]);
     }
+    private Biome[] StarSkppirspawn = {
+            Biomes.OCEAN, Biomes.PLAINS, Biomes.DESERT, Biomes.EXTREME_HILLS, Biomes.FOREST, Biomes.TAIGA, Biomes.SWAMPLAND, Biomes.FROZEN_OCEAN, Biomes.FROZEN_RIVER, Biomes.ICE_MOUNTAINS, Biomes.DESERT_HILLS, Biomes.FOREST_HILLS, Biomes.TAIGA_HILLS, Biomes.JUNGLE, Biomes.JUNGLE_HILLS, Biomes.JUNGLE_EDGE, Biomes.DEEP_OCEAN, Biomes.STONE_BEACH, Biomes.COLD_BEACH, Biomes.BIRCH_FOREST, Biomes.BIRCH_FOREST_HILLS, Biomes.ROOFED_FOREST, Biomes.REDWOOD_TAIGA, Biomes.REDWOOD_TAIGA_HILLS, Biomes.EXTREME_HILLS_WITH_TREES, Biomes.SAVANNA, Biomes.MESA, Biomes.MESA_ROCK, Biomes.MESA_CLEAR_ROCK, Biomes.MUTATED_PLAINS, Biomes.MUTATED_DESERT, Biomes.MUTATED_EXTREME_HILLS, Biomes.MUTATED_FOREST, Biomes.MUTATED_TAIGA, Biomes.MUTATED_SWAMPLAND, Biomes.MUTATED_ICE_FLATS, Biomes.MUTATED_JUNGLE, Biomes.MUTATED_JUNGLE_EDGE, Biomes.MUTATED_BIRCH_FOREST, Biomes.MUTATED_BIRCH_FOREST_HILLS, Biomes.MUTATED_ROOFED_FOREST, Biomes.MUTATED_TAIGA_COLD, Biomes.MUTATED_REDWOOD_TAIGA, Biomes.MUTATED_REDWOOD_TAIGA_HILLS, Biomes.MUTATED_EXTREME_HILLS_WITH_TREES, Biomes.MUTATED_SAVANNA, Biomes.MUTATED_SAVANNA_ROCK, Biomes.MUTATED_MESA, Biomes.MUTATED_MESA_ROCK, Biomes.MUTATED_MESA_CLEAR_ROCK,
+    };
 
     public void preInit(FMLPreInitializationEvent event){
         musicInit.registerSounds();
@@ -34,7 +38,7 @@ public class commonProxy {
     }
     public void init(FMLInitializationEvent event){
         Biome[] spawnBiomes = allbiomes(Biome.REGISTRY);
-        EntityRegistry.addSpawn(EntityStarSkipper.class, 20, 2, 6, EnumCreatureType.MONSTER, spawnBiomes);
+        EntityRegistry.addSpawn(EntityStarSkipper.class, 20, 2, 6, EnumCreatureType.MONSTER, StarSkppirspawn);
 
     }
     public void postInit(FMLPostInitializationEvent event){
