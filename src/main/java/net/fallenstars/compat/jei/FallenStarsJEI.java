45eba86f6de68668d4fa540a019b47146a9fd324
RecallGGs <recallggs@gmail.com>
2020-09-07 18:30:24 -0500

    Started JEI integration

--- src/main/java/net/fallenstars/compat/jei/FallenStarsJEI.java
+++ src/main/java/net/fallenstars/compat/jei/FallenStarsJEI.java
@@ -1,9 +1,27 @@
 package net.fallenstars.compat.jei;
 
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.*;
+import mezz.jei.api.ingredients.IModIngredientRegistration;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+
 
 @JEIPlugin
 public class FallenStarsJEI implements IModPlugin {
+    public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
+    }
+
+    public void registerIngredients(IModIngredientRegistration registry) {
+    }
+
+    public void registerCategories(IRecipeCategoryRegistration registry) {
+
+    }
+
+    public void register(IModRegistry registry) {
+    }
+
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+
+    }
 
 }
--- src/main/java/net/fallenstars/compat/jei/startable/StarTableDec.java
+++ src/main/java/net/fallenstars/compat/jei/startable/StarTableDec.java
@@ -1,4 +1,5 @@
 package net.fallenstars.compat.jei.startable;
 
 public class StarTableDec {
+
 }
